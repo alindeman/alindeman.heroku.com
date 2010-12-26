050b245f40d0240c3b6e059a14dccaf3604fed1d
Andy Lindeman <alindeman@gmail.com>
2010-12-26 09:08:06 -0600

    Create simple application to redirect to my blog on github pages

---
+++ config.ru
@@ -0,0 +1,2 @@
+require 'redirect'
+run Sinatra::Application
---
+++ redirect.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'sinatra'
+
+get %r{.*} do
+  redirect "http://alindeman.github.com#{params[:captures].first}"
+end
