60f74c18b771293b7c69847a736cd27c266ac645
Andy Lindeman <alindeman@gmail.com>
2010-12-26 16:41:29 -0600

    Add /s3 directory to redirect to my S3 bucket

--- redirect.rb
+++ redirect.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'sinatra'
 
-get %r{(.*)} do
+get %r{^/s3/(.*)$} do
+  redirect "https://alindeman.s3.amazonaws.com/#{params[:captures].first}", 301 # moved permanently
+end
+
+get %r{^(.*)$} do
   redirect "http://alindeman.github.com#{params[:captures].first}", 301 # moved permanently
 end
