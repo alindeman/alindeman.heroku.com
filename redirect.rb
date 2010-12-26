a6b6cedc44650312abc08270b8909ed88d704f81
Andy Lindeman <alindeman@gmail.com>
2010-12-26 11:13:44 -0600

    Use HTTP 301 code

--- redirect.rb
+++ redirect.rb
@@ -2,5 +2,5 @@ require 'rubygems'
 require 'sinatra'
 
 get %r{(.*)} do
-  redirect "http://alindeman.github.com#{params[:captures].first}"
+  redirect "http://alindeman.github.com#{params[:captures].first}", 301 # moved permanently
 end
