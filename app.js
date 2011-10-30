2858147a9c986ddb80b18f212207802150a31a6c
Andy Lindeman <andy@highgroove.com>
2011-10-30 00:06:19 -0400

    Redirects to S3 bucket if URL is prefixed with /s3

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var http = require("http");
 
 http.createServer(function(req, res) {
-  res.writeHead(301, {"Location": "http://alindeman.github.com" + req.url});
+  var redirect = "";
+  var matches;
+
+  if (matches = req.url.match(/^\/s3(.*)$/))
+    redirect = "https://alindeman.s3.amazonaws.com" + matches[1];
+  else
+    redirect = "http://alindeman.github.com" + req.url
+
+  res.writeHead(301, {"Location": redirect});
   res.end();
 }).listen(process.env.PORT || 3000);
